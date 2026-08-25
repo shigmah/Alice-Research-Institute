0172f7cb9802486c3d2648f33e4583ad9647b793
shigmah <fi8e34mnk@gmail.com>
2026-08-25 23:37:55 +0900

    feat: bootstrap mode selection UI

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,11 @@
 import { GameController } from "./GameController.js";
 import { MainScreen } from "../ui/MainScreen.js";
+import { ensureModeSetup } from "../ui/ModeSetup.js";
 import { Game } from "../core/Game.js";
 
 export function createMain(documentRef = document) {
+  ensureModeSetup(documentRef);
+
   const game = new Game();
   const ui = new MainScreen(documentRef);
   const controller = new GameController({ game, ui });
