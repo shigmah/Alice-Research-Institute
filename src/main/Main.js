69c00a3a6e87c7a40d9e6992d7d1635a60faf5f8
shigmah <fi8e34mnk@gmail.com>
2026-08-29 23:02:40 +0900

    feat: wire Battle setup controls into main UI

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,14 +1,17 @@
 import { GameController } from "./GameController.js";
 import { MainScreen } from "../ui/MainScreen.js";
 import { ensureModeSetup, installCollectorModeSupport } from "../ui/ModeSetup.js";
+import { ensureBattleSetup, installBattleModeSupport } from "../ui/BattleSetup.js";
 import { Game } from "../core/Game.js";
 
 export function createMain(documentRef = document) {
   ensureModeSetup(documentRef);
+  ensureBattleSetup(documentRef);
 
   const game = new Game();
   const ui = new MainScreen(documentRef);
   installCollectorModeSupport(ui);
+  installBattleModeSupport(ui);
   const controller = new GameController({ game, ui });
 
   return { game, ui, controller };
