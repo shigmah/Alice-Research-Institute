a9bf8ba099f9b9807317ccae200d0c40f4e40f17
shigmah <fi8e34mnk@gmail.com>
2026-08-31 17:25:36 +0900

    test: validate Battle end with independent player states

--- tests/battle-end-integration-test.js
+++ tests/battle-end-integration-test.js
@@ -11,41 +11,47 @@ function prepareBattle() {
 
 test("Battle ends after both players drop out and judges the larger fixed cat count", () => {
   const game = prepareBattle();
-  const p1 = game.battleMode.player1;
-  const p2 = game.battleMode.player2;
+  const battle = game.battleMode;
+  const p1 = battle.player1;
+  const p2 = battle.player2;
+  const p1Context = battle.getPlayerContext(p1);
+  const p2Context = battle.getPlayerContext(p2);
 
-  p1.getAction = () => ({ type: "CONTINUE" });
-  p2.getAction = () => ({ type: "CONTINUE" });
+  p1.setAction({ type: "CONTINUE" });
+  p2.setAction({ type: "CONTINUE" });
 
-  game.catManager.createCat();
-  game.catManager.createCat();
+  p1Context.catManager.createCat();
+  p1Context.catManager.createCat();
+  p1Context.randomManager.rollDice = () => 1;
+
   const firstTurn = game.roll();
-  const catsAfterFirstTurn = game.state.getCats().length;
+  const catsAfterFirstTurn = p1Context.state.getCats().length;
   assert.equal(firstTurn?.mode?.player, p1);
   assert.equal(game.state.turn, 2);
   assert.ok(catsAfterFirstTurn >= 2);
 
-  p2.getAction = () => ({ type: "DROP_OUT" });
+  p2Context.catManager.createCat();
+  p2.setAction({ type: "DROP_OUT" });
   const secondTurn = game.roll();
   assert.equal(secondTurn?.mode?.action?.type, "DROP_OUT");
   assert.equal(p2.isDroppedOut(), true);
-  assert.equal(p2.getFixedCatCount(), catsAfterFirstTurn);
+  assert.equal(p2.getFixedCatCount(), p2Context.state.getCats().length);
   assert.equal(game.state.isGameOver, false);
   assert.equal(game.state.turn, 3);
 
-  game.catManager.createCat();
-  const catsBeforeThirdTurn = game.state.getCats().length;
+  p1Context.catManager.createCat();
+  const catsBeforeThirdTurn = p1Context.state.getCats().length;
   assert.equal(catsBeforeThirdTurn, catsAfterFirstTurn + 1);
-  p1.getAction = () => ({ type: "DROP_OUT" });
+  p1.setAction({ type: "DROP_OUT" });
   const thirdTurn = game.roll();
 
   assert.equal(thirdTurn?.mode?.action?.type, "DROP_OUT");
   assert.equal(p1.isDroppedOut(), true);
   assert.equal(p1.getFixedCatCount(), catsBeforeThirdTurn);
-  assert.equal(p1.getFixedCatCount(), p2.getFixedCatCount() + 1);
+  assert.equal(p1.getFixedCatCount() > p2.getFixedCatCount(), true);
   assert.equal(game.state.isGameOver, true);
   assert.equal(thirdTurn?.mode?.battleResult?.winner, p1);
-  assert.equal(game.battleMode.battleResult?.winner, p1);
+  assert.equal(battle.battleResult?.winner, p1);
 });
 
 test("A completed battle does not execute another player turn", () => {
