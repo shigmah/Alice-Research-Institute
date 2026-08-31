009d6145964d17acd7aaf4efcdfae6a6d3c115af
shigmah <fi8e34mnk@gmail.com>
2026-08-31 13:56:17 +0900

    feat: install side-by-side Battle player board

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,6 +2,7 @@ import { GameController } from "./GameController.js";
 import { MainScreen } from "../ui/MainScreen.js";
 import { ensureModeSetup, installCollectorModeSupport } from "../ui/ModeSetup.js";
 import { ensureBattleSetup, installBattleModeSupport } from "../ui/BattleSetup.js";
+import { installBattlePlayerBoard } from "../ui/BattleBoard.js";
 import { Game } from "../core/Game.js";
 
 export function createMain(documentRef = document) {
@@ -12,6 +13,7 @@ export function createMain(documentRef = document) {
   const ui = new MainScreen(documentRef);
   installCollectorModeSupport(ui);
   installBattleModeSupport(ui);
+  installBattlePlayerBoard(ui);
   const controller = new GameController({ game, ui });
 
   return { game, ui, controller };
