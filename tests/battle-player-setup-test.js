5ace4261b4e130ac9120f2f4474443aaf57a1dc8
shigmah <fi8e34mnk@gmail.com>
2026-08-31 17:24:27 +0900

    test: update Battle player setup expectations

--- tests/battle-player-setup-test.js
+++ tests/battle-player-setup-test.js
@@ -7,20 +7,33 @@ import EasyStrategy from "../src/ai/strategy/EasyStrategy.js";
 import NormalStrategy from "../src/ai/strategy/NormalStrategy.js";
 import HardStrategy from "../src/ai/strategy/HardStrategy.js";
 
-test("Game configures a human Player and an Easy NPC by default", () => {
+test("Game configures a human Player and an Easy NPC with independent contexts by default", () => {
   const game = new Game();
   game.startBattleMode();
 
   const battle = game.battleMode;
-  assert.ok(battle.player1 instanceof Player);
-  assert.ok(battle.player2 instanceof NpcPlayer);
-  assert.equal(battle.player1.currentState, game.state);
-  assert.equal(battle.player2.currentState, game.state);
-  assert.equal(battle.player1.playRule, game.currentRule);
-  assert.equal(battle.player2.playRule, game.currentRule);
-  assert.equal(battle.player2.difficulty, "easy");
-  assert.ok(battle.player2.npcAI);
-  assert.ok(battle.player2.npcAI.getStrategy() instanceof EasyStrategy);
+  const human = battle.player1;
+  const npc = battle.player2;
+  const humanContext = battle.getPlayerContext(human);
+  const npcContext = battle.getPlayerContext(npc);
+
+  assert.ok(human instanceof Player);
+  assert.ok(npc instanceof NpcPlayer);
+  assert.ok(humanContext);
+  assert.ok(npcContext);
+  assert.notEqual(humanContext.state, npcContext.state);
+  assert.notEqual(humanContext.catManager, npcContext.catManager);
+  assert.notEqual(humanContext.randomManager, npcContext.randomManager);
+  assert.notEqual(humanContext.playRule, npcContext.playRule);
+  assert.equal(human.currentState, humanContext.state);
+  assert.equal(npc.currentState, npcContext.state);
+  assert.equal(human.playRule, humanContext.playRule);
+  assert.equal(npc.playRule, npcContext.playRule);
+  assert.equal(humanContext.state.getGameMode(), "CLASSIC");
+  assert.equal(npcContext.state.getGameMode(), "CLASSIC");
+  assert.equal(npc.difficulty, "easy");
+  assert.ok(npc.npcAI);
+  assert.ok(npc.npcAI.getStrategy() instanceof EasyStrategy);
 });
 
 test("Game selects the requested NPC difficulty and strategy", () => {
